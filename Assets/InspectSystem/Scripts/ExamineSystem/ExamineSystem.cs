772c741b280ef0877f64440cd5f66e705c3bb79e
chadlhendricks <chadlhendricks@gmail.com>
2023-12-24 18:23:29 +0200

    Removed unnecessary scripts

--- Assets/InspectSystem/Scripts/ExamineSystem/ExamineSystem.cs
+++ Assets/InspectSystem/Scripts/ExamineSystem/ExamineSystem.cs
@@ -5,7 +5,7 @@ using UnityTutorial.PlayerControl;
 public class ExamineSystem : MonoBehaviour
 {
     [SerializeField] private GameObject Examine_Point;
-    [SerializeField] private GameObject VFX_Effect;
+    // [SerializeField] private GameObject VFX_Effect;
     [SerializeField] private GameObject info_UI;
 
     [SerializeField] private PlayerInventory playerInventory;
@@ -32,7 +32,7 @@ public class ExamineSystem : MonoBehaviour
     public void Start()
     {
         Examine_Point = GameObject.Find("ExaminePoint");
-        VFX_Effect = GameObject.Find("DoF-Effect");
+        // VFX_Effect = GameObject.Find("DoF-Effect");
         Inventory_UI = GameObject.Find("Panel");
         info_UI = GameObject.Find("Info_UI");
         _cursorIcon = FindObjectOfType<CursorIcon>();
@@ -44,7 +44,7 @@ public class ExamineSystem : MonoBehaviour
         parentSize = Examine_Point.transform.GetComponent<BoxCollider>().bounds.size / 2;
         info_UI.GetComponentInChildren<Button>(true).onClick.AddListener(ExitExamineMode);
         info_UI.SetActive(false);
-        VFX_Effect.SetActive(false);
+        // VFX_Effect.SetActive(false);
         Inventory_UI.SetActive(false);
         ExamineMode = false;
     }
@@ -53,7 +53,7 @@ public class ExamineSystem : MonoBehaviour
     {
         ExamineMode = true;
         info_UI.SetActive(true);
-        VFX_Effect.SetActive(true);
+        // VFX_Effect.SetActive(true);
         Examine_Object = item;
         ItemInspect(Examine_Object);
     }
@@ -73,7 +73,7 @@ public class ExamineSystem : MonoBehaviour
             TooltipSystem.Hide();
             ExamineMode = true;
             info_UI.SetActive(true);
-            VFX_Effect.SetActive(true);
+            // VFX_Effect.SetActive(true);
             Examine_Object = Inspect_object;
             Examine_Object.SetActive(true);
 
@@ -128,7 +128,7 @@ public class ExamineSystem : MonoBehaviour
 
         this.gameObject.GetComponent<PlayerController>().enabled = true;
         ExamineMode = false;
-        VFX_Effect.SetActive(false);
+        // VFX_Effect.SetActive(false);
         info_UI.SetActive(false);
 
 
@@ -165,7 +165,7 @@ public class ExamineSystem : MonoBehaviour
     #region Resize
     public void ResizeObject(GameObject Resize_object)
     {
-        MainCamera.transform.localPosition = new Vector3(0, 0.8f, 0);
+        MainCamera.transform.localPosition = new Vector3(0, 1.657f, 0.27f);
         MainCamera.localRotation = new Quaternion(0, 0, 0, 0);
 
 
